19631ab643a51aac562503fbb7c155d3472a4fb2
ChrlyDev <cf9220543@gmail.com>
2024-12-31 16:48:37 -0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+//Función para ACEPTAR/ENVIAR Nts debito-->
+function envianot() {
+  document.getElementById("NOTASDB").style.display = "none";
+}
+//Función para SALIR Nts debito-->
+function nsale() {
+  document.getElementById("NOTASDB").style.display = "none";
+}
+// Función para realizar cobro-->
+function cobrar() {
+  document.getElementById("COBRARF").style.display = "flex";
+}
+// Función para liquidar facturar-->
+function liquidar() {
+  document.getElementById("COBRARF").style.display = "none";
+}
+// Función para Notas Debito-->
+function ndebito() {
+  document.getElementById("NOTASDB").style.display = "flex";
+}
+  
